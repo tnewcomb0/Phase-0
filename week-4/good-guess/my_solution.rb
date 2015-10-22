34893e3d06d8c0f4040fe45c88abf081341af160
Trevor Newcomb <trevornewcomb@Trevors-MacBook-Pro.local>
2015-10-22 10:16:55 -0700

    Add good guess

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,14 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def good_guess? (number)
+  if number == 42
+    return true
+  else
+    return false
+  end
+end
