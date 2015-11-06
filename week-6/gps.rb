cac2997a7d8d41f92ed7dc2c679f5589812e58f4
Trevor Newcomb <trevornewcomb@Trevors-MacBook-Pro.local>
2015-11-06 07:48:32 -0800

    Add initial gps assignment

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,33 +1,38 @@
 # Your Names
-# 1)
-# 2)
+# 1) Nil Thacker
+# 2) Trevor Newcomb
 
-# We spent [#] hours on this challenge.
+# We spent [1.75] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, order_quantity)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
+  library = {"cookie" => 1, "cake" =>  5, "pie" => 7, "brownie" => 4, "cupcake" => 3}
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
-  end
+  #make sure item_to_make is in library, otherwise raise argumenterror
+  raise ArgumentError.new("#{item_to_make} is not a valid input") unless library.has_key?(item_to_make)
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
+  required_ingredients = library[item_to_make]
+  leftover_ingredients = order_quantity % required_ingredients
 
-  serving_size = library.values_at(item_to_make)[0]
-  serving_size_mod = order_quantity % serving_size
-
-  case serving_size_mod
+  case leftover_ingredients
   when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+    return "Calculations complete: Make #{order_quantity/required_ingredients} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+    suggested_items = ""
+    sorted = library.sort_by {|key, value| value}.reverse
+    leftovers = leftover_ingredients
+
+    # if leftovers goes evenly into the current item, print how many times it goes evenly into the item and subtract that amount from leftovers
+    sorted.each do |sub_array|
+      if (leftovers / sub_array[1]) > 0
+        suggested_items += "#{leftovers / sub_array[1]} #{sub_array[0]}(s) "
+        leftovers -= (leftovers / sub_array[1]) * sub_array[1]
+      end
+    end
+
+
+    return "Calculations complete: Make #{order_quantity/required_ingredients} of #{item_to_make}, you have #{leftover_ingredients} leftover ingredients. Suggested baking items: #{suggested_items}"
   end
 end
 
@@ -40,4 +45,18 @@ p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+=begin
 
+What did you learn about making code readable by working on this challenge?
+  asdf
+
+Did you learn any new methods? What did you learn about them?
+  asdf
+
+What did you learn about accessing data in hashes?
+  asdf
+
+What concepts were solidified when working through this challenge?
+  asdf
+
+=end
