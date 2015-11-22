fc8e1ceb53e7d928a051b5ccd2d4fe697c300fd5
Trevor Newcomb <trevornewcomb@Trevors-MacBook-Pro.local>
2015-11-22 12:28:48 -0800

    Add 8.6

---
+++ week-8/ruby/ruby_review.rb
@@ -0,0 +1,43 @@
+# Reverse Words
+
+
+# I worked on this challenge by myself.
+# This challenge took me [2] hours.
+
+# Pseudocode
+
+# First, I need to split the string into individual words.
+# Then, I need to iterate over the array and reverse each word.
+# Then, combine all the words back into a sentence.
+
+
+# Initial Solution
+
+# def reverse_words(str)
+#   words = str.split(' ')
+#   reverse_string = []
+
+#   words.length.times do |i|
+#     reverse_string[i] = words[i].reverse
+#   end
+
+#   return reverse_string.join(" ")
+# end
+
+# Refactored Solution
+
+# I couldn't find any way to shorten this.
+
+# Reflection
+
+# What concepts did you review or learn in this challenge?
+
+# I reviewed a simple concept to get me back in the flow of using ruby again. It's a little weird changing between languages when you are getting used to one.
+
+# What is still confusing to you about Ruby?
+
+# Sometimes looping in certain ways still confuses me.
+
+# What are you going to study to get more prepared for Phase 1?
+
+# I'm going to do more of the challenges when I get the chance.
